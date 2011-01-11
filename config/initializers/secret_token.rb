1b46242c4ee97d552202f49ff73db3652e2dc388
Joh Hoskin <joh@dev.geek.nz>
2011-01-11 14:37:49 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FatYak::Application.config.secret_token = 'd2161e01662dc094b5a6752f2fc2da50bc059500845569f321c3e426d5d7e75be3265282ffec66f3702c4dbdac9ee942c6cb661be436574a0a661cf4c9b5cf66'
