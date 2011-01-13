bcd801179c0ac679c0472cf2550d6e3ad191e660
Joh Hoskin <joh@dev.geek.nz>
2011-01-13 13:12:10 +1100

    Added remaining associations

--- app/models/ingredient_batch.rb
+++ app/models/ingredient_batch.rb
@@ -1,3 +1,5 @@
 class IngredientBatch < ActiveRecord::Base
 	belongs_to :order
+	belongs_to :ingredient
+	belongs_to :quantity_unit, :class_name => 'MeasurementUnit'
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ActiveRecord::Base
+	belongs_to :sellable_yield_unit, :class_name => 'MeasurementUnit'
+	belongs_to :waste_yield_unit, :class_name => 'MeasurementUnit'
+	has_many :recipe_ingredients
+	has_many :ingredients, :through => :recipe_ingredients
 end
