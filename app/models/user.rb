6c4cc4061a25388955a619f5747d8e393a291ea0
Joh Hoskin <joh@dev.geek.nz>
2011-01-17 14:46:00 +1100

    Lock down user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 class User < ActiveRecord::Base
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+	attr_accessible :name, :email
+
 	validates :name,	:presence => true
 	validates :email,	:presence => true,
 						:format => {:with => email_regex}
